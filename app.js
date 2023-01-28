dcb348a123aa9940f25341f824704747d75eb7be
Daniela Passos <moitidani@gmail.com>
2023-01-28 13:03:28 -0300

    Updating serverless host

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ app.post("/success", function(req, res){
 
 })
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000")
+app.listen(function(){
+    console.log("Server is running!")
 })
